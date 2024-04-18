4fa7dc1098b2c2ae0be22c9ba3aa81e44f411e50
Juan Croppi <juancroppi@hotmail.com>
2024-04-17 22:40:57 -0300

    Actualizo infoasdsad

--- js/script.js
+++ js/script.js
@@ -12,30 +12,3 @@ function togglePlay() {
         iconoPlay.classList.add("fa-play");
     }
 }
-
-function saveForm() {
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var phoneNumber = document.getElementById('phoneNumber').value;
-    var websiteUrl = document.getElementById('websiteUrl').value;
-    var message = document.getElementById('message').value;
-
-    var formData = "Nombre: " + name + "\n" +
-        "Correo electrónico: " + email + "\n" +
-        "Teléfono: " + phoneNumber + "\n" +
-        "Página web: " + websiteUrl + "\n" +
-        "Mensaje: " + message;
-
-    var file = new Blob([formData], { type: 'text/plain' });
-
-    var a = document.createElement("a");
-    var url = URL.createObjectURL(file);
-    a.href = url;
-    a.download = 'formulario.txt';
-    document.body.appendChild(a);
-    a.click();
-    setTimeout(function() {
-        document.body.removeChild(a);
-        window.URL.revokeObjectURL(url);
-    }, 0);
-}
